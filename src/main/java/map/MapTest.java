e3dfdfae54bcfd0df4af52deb1fadb5c5cbf29b2
zluo <luozhanqing@gmail.com>
2010-11-24 14:29:40 -0500

    First Commit, HashMap, HashTable, HashSet study

---
+++ src/main/java/map/MapTest.java
@@ -0,0 +1,23 @@
+package map;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+public class MapTest extends TestCase {
+
+/**
+ * HashSet is a special use of HashMap, it use the item as the HashMap's key.
+ * 
+ */
+	 public void testHashSet()
+	{
+		Set set=new HashSet();
+		set.add("1");
+		set.add("1");
+	}
+	 
+	 
+
+}
