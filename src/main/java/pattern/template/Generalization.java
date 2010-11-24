704ca6d48c93946370c31ef1b884dc77e536741d
zluo <luozhanqing@gmail.com>
2010-11-24 15:23:21 -0500

    add Template Method Pattern example

---
+++ src/main/java/pattern/template/Generalization.java
@@ -0,0 +1,50 @@
+package pattern.template;
+
+/**
+ *  <pre>
+ *  Template Method Pattern
+ *  
+ *  1. Standardize the skeleton of an algorithm in a base class's "template" method.
+ *  
+ *  in this example: 
+ *  <li>base class is: <code>Generalization</code> class</li>
+ *  <li>template method is: <code>solution</code> method.</li>
+ *  
+ *  2. Common implementations of individual steps are defined in the base class
+ *    
+ *    in this example: 
+ *      <code>stepOne</code> and <code>stepTwo</code> is the common implementations
+ *    
+ *  3. Steps requiring peculiar implementations are "placeholders" in base class.
+ *      <code>stepThree</code> and <code>stepFour</code> is the abstract method as the placeholder
+ *  
+ *  
+ * @author zluo
+ *
+ */
+
+public abstract class Generalization 
+{
+ 
+	public void solution()
+   {
+	 stepOne();
+	 stepTwo();
+	 stepThree();
+	 stepFour();
+   }
+	
+	protected void stepOne()
+	{
+		System.out.println(Generalization.class.getName() + ".setpOne");
+	}
+	
+	abstract protected void stepTwo();
+	abstract protected void stepThree();
+
+	protected void stepFour()
+	{
+		System.out.println(Generalization.class.getName() + ".setpFour");
+	}
+	
+}
