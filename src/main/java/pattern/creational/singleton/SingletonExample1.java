95e9405664da5c181094c8fddc202dd10b68fa6d
zluo <luozhanqing@gmail.com>
2011-01-14 08:04:03 -0800

    add singleton example

---
+++ src/main/java/pattern/creational/singleton/SingletonExample1.java
@@ -0,0 +1,17 @@
+package pattern.creational.singleton;
+/**
+ * First style of singleton example, has a private static instance
+ * @author zluo
+ *
+ */
+
+public class SingletonExample1 {
+	
+	private static SingletonExample1 = new SingletonExample1();
+	
+	static public SingletonExample1 getInstance()
+	{
+		
+	}
+
+}
