906029a4d380b5fe21f6dff011c67509d41af18f
zluo <luozhanqing@gmail.com>
2014-03-14 09:00:48 -0400

    add <=> operator examples
    Change-Id: Ie0666e5c0fe9b1f63839ab32592c24f98f0a879a

---
+++ src/main/groovy/example/sort/sortMapbyValue.groovy
@@ -0,0 +1,27 @@
+package example.sort
+
+/**
+ * what's the <=> mean? 
+ * <=> spacesship operator, original from Perl
+ * 
+ * examples: 
+ * 
+ * a= (5 <=> 7);  # $a is set to -1
+ * a = (7 <=> 5);  # $a is set to 1
+ * 6 <=> 6;  # $a is set to 0
+ * 
+ * @see http://stackoverflow.com/questions/12574669/groovy-sort-with-comparator-syntax
+ * @author zluo
+ *
+ */
+
+def map=[a:1, b:5, c:4]
+
+def map1 =map.sort{a,b -> a.value <=> b.value}
+
+println map1.toMapString()
+
+
+assert -1 == (5 <=> 7)
+assert 1 == (7 <=>5)
+assert 0==(6 <=>6)
