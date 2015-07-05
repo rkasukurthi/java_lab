98cf5029765cdc6f2528fe43e888565d273265a0
zluo <luozhanqing@gmail.com>
2015-07-05 15:33:13 -0400

    fix error in treenode

--- src/main/java/algorithm/leetcode/TreeNode.java
+++ src/main/java/algorithm/leetcode/TreeNode.java
@@ -1,34 +1,35 @@
 package algorithm.leetcode;
 
 public class TreeNode {
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-    
-    public TreeNode(int x) {
-	val = x;
-    }
-    
-    public static TreeNode insert(TreeNode root, int val) {
-      TreeNode newNode = new TreeNode(val);
-      if (root==null) return newNode;
-      TreeNode current =root;
-      TreeNode parent;
-      while(true) {
-        parent =current;
-        if (val < current.val) {
-          current = current.left;
-          if (current ==null) {
-            parent.left=newNode;
-            return root;
-          }
-        }else {
-          current =current.right;
-          if (current ==null) {
-            parent.right=newNode;
-            return root;
-          }
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
+
+  public TreeNode(int x) {
+    val = x;
+  }
+
+  public static TreeNode insert(TreeNode root, int val) {
+    TreeNode newNode = new TreeNode(val);
+    if (root == null)
+      return newNode;
+    TreeNode current = root;
+    TreeNode parent;
+    while (true) {
+      parent = current;
+      if (val < current.val) {
+        current = current.left;
+        if (current == null) {
+          parent.left = newNode;
+          return root;
+        }
+      } else {
+        current = current.right;
+        if (current == null) {
+          parent.right = newNode;
+          return root;
         }
       }
     }
+  }
 }
